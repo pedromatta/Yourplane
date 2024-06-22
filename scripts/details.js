38816e1af772dfadcd4e19b00d91d7e6d3bf75bc
pedromatta <pedromatta8200@gmail.com>
2024-06-21 23:10:16 -0300

    Finalizando tela de detalhes das aeronaves

--- scripts/details.js
+++ scripts/details.js
@@ -9,6 +9,7 @@ import {
 
 const navbarRightSide = document.getElementById('navbarNavAltMarkup');
 const carouselItemDiv = document.getElementById('carousel-item-div');
+const detailsInformation = document.getElementById('details-information');
 
 function getElementsCallFunctions(){
     const airplanes = loadAirplanesFromLocalStorage();
@@ -16,6 +17,7 @@ function getElementsCallFunctions(){
 
     renderDetailsNavbarRightSide(airplanes, currentPlane);
     renderDetailsCarousel(currentPlane);
+    renderDetailsInformation(currentPlane);
 }
 
 function getPageIdFromUrl() {
@@ -113,9 +115,69 @@ function renderDetailsCarousel(currentPlane){
             <h5>${currentPlane.model}</h5>
             <p class="d-none d-md-block">${currentPlane.short_description}</p>
             </div>
-        </div>`
+        </div>`;
 
     }
+}
+
+function renderDetailsInformation(currentPlane){
+    const description = renderDescription(currentPlane);
+    const specifications = renderSpecifications(currentPlane);
+    const intel = renderIntel(currentPlane);
+    detailsInformation.innerHTML = `
+        <div class="destaques-container px-4 col">
+            ${description}
+            ${specifications}
+        </div>
+        <div class="container col">
+            ${intel}
+        </div>`
+}
+
+function renderDescription(currentPlane) {
+    return `<h4 style="color: var(--black);">Descrição
+                <hr />
+            </h4>
+            <p class="text-justify">${currentPlane.description}</p>`
+}
+
+function renderSpecifications(currentPlane) {
+    return `
+        <div class="card specifications-card">
+            <button class="btn-specifications card-header my-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-card">Especificações</button>
+            <div class="collapse card-body" id="collapse-card">
+                <p class="card-text"><b>Largura: </b>${currentPlane.specifications.length}</p>
+                <p class="card-text"><b>Envergadura: </b>${currentPlane.specifications.wingspan}</p>
+                <p class="card-text"><b>Altura: </b>${currentPlane.specifications.height}</p>
+                <p class="card-text"><b>Peso Vazio: </b>${currentPlane.specifications.empty_weight}</p>
+                <p class="card-text"><b>Capacidade Máxima: </b>${currentPlane.specifications.max_takeoff_weight}</p>
+                <p class="card-text"><b>Motores: </b>${currentPlane.specifications.engines}</p>
+            </div>
+        </div>`
+}
+
+function renderIntel(currentPlane) {
+    return `
+        <div class="card details-card h-auto mb-3">
+            <div class="card-text details-card-text">Modelo</div>
+            <div class="card-body details-card-body">${currentPlane.model}</div>
+        </div>
+        <div class="card details-card h-auto mb-3">    
+            <div class="card-text details-card-text">Fabricante</div>
+            <div class="card-body details-card-body">${currentPlane.manufacturer}</div>
+        </div>
+        <div class="card details-card h-auto mb-3">
+            <div class="card-text details-card-text">Capacidade</div>
+            <div class="card-body details-card-body">${currentPlane.capacity} Passageiros</div>
+        </div>
+        <div class="card details-card h-auto mb-3">
+            <div class="card-text details-card-text">Alcance</div>
+            <div class="card-body details-card-body">${currentPlane.range}</div>
+        </div>    
+        <div class="card details-card h-auto mb-3">
+            <div class="card-text details-card-text">Velocidade</div>
+            <div class="card-body details-card-body">${currentPlane.cruise_speed}</div>
+        </div>`
 }
 
 window.onload = () =>{
